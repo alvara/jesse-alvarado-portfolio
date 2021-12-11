c8e565516f97d5ab1814a1a781b7a78ed32d7e0f
Jesse Alvarado <contact@jessealvarado.com>
2021-12-11 18:31:31 +0900

    make sections same height as page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import logo from './logo.svg';
 import './App.scss';
 import Navbar from './components/Navbar/Navbar';
+import Portfolio from './components/Portfolio/Portfolio';
+import Contact from './components/Contact/Contact';
+import Blog from './components/Blog/Blog';
+import Testimonials from './components/Testimonials/Testimonials';
+import Header from './components/Header/Header';
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
-      
-      sec - intro -portfolio -work .. testimonials
+      <div className='sections'>
+      <Header/>
+      <Portfolio/>
+      <Blog/>
+      <Testimonials/>
+      <Contact/>
+      </div>
     </div>
   );
 }
