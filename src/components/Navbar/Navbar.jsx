7989d54861fdd0def959866f6987264f8567b1ca
Jesse Alvarado <contact@jessealvarado.com>
2021-12-11 20:28:05 +0900

    setup icons and hamburger lines

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,4 +1,5 @@
 import './Navbar.scss';
+import { GitHub, LinkedIn } from '@material-ui/icons';
 
 export default function Navbar() {
   return (
@@ -7,11 +8,20 @@ export default function Navbar() {
         <div className="left">
           <a href="#header" className='logo'><h1>Jesse Alvarado</h1></a>
           <div className="item-container">
-            <a href="#about" className="item">Contact: contact@jessealvarado.com</a>
+            <a><LinkedIn className='icon'/></a>
+          </div>
+          <div className="item-container">
+            <a><GitHub className='icon'/></a>
           </div>
       
         </div>
         <div className="right">
+          <div className="hamburger">
+            <span className="line1"></span>
+            <span className="line2"></span>
+            <span className="line3"></span>
+          </div>
+
           <a href="#portfolio">Portfolio</a>
           <a href="#blog">Blog</a>
           <a href="#contact">Contact</a>
