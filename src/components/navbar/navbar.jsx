b60bab4cfcf5dd7b69a6b66d3823b65f509865b5
Jesse Alvarado <contact@jessealvarado.com>
2021-12-12 07:39:38 +0900

    restyle nav height

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,18 +1,13 @@
 import './Navbar.scss';
 import Socials from '../Socials/Socials';
 
+
 export default function Navbar({ menuOpen, setMenuOpen }) {
   return (
     <div className={'navbar ' + (menuOpen && "active") } id='navbar'>
       <div className='wrapper'>
         <div className="left">
           <a href="#header" className='logo'><span>Jesse Alvarado</span></a>
-          <div className="item-container">
-            <a><LinkedIn className='icon'/></a>
-          </div>
-          <div className="item-container">
-            <a><GitHub className='icon'/></a>
-          </div>
         </div>
         <div className="right">
           <Socials />
