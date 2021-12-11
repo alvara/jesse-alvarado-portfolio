e17216ca58d5c226bc82e0fe98607a650f78e933
Jesse Alvarado <contact@jessealvarado.com>
2021-12-11 19:43:18 +0900

    setup nav layout

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -5,12 +5,17 @@ export default function Navbar() {
     <div className='navbar' id='navbar'>
       <div className='wrapper'>
         <div className="left">
-          <a href="/">Jesse Alvarado</a>
+          <a href="#header" className='logo'><h1>Jesse Alvarado</h1></a>
+          <div className="item-container">
+            <a href="#about" className="item">Contact: contact@jessealvarado.com</a>
+          </div>
+      
+        </div>
+        <div className="right">
           <a href="#portfolio">Portfolio</a>
           <a href="#blog">Blog</a>
           <a href="#contact">Contact</a>
         </div>
-        <div className="right"></div>
       </div>
     </div>
   )
