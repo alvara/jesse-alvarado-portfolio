828c0dd1a815368f1a12e39ff0a3b44c06c9ca34
Jesse Alvarado <contact@jessealvarado.com>
2021-12-13 17:16:55 +0900

    update contact

--- src/components/Socials/Socials.jsx
+++ src/components/Socials/Socials.jsx
@@ -5,10 +5,10 @@ export default function Socials() {
   return (
     <div className='socials hide'>
       <div className="item-container">
-        <a href='https://www.linkedin.com/in/jesse-alvarado/'><LinkedIn className='icon'/></a>
+        <a href='https://www.linkedin.com/in/jesse-alvarado/' target="_blank"><LinkedIn className='icon'/></a>
       </div>
       <div className="item-container">
-        <a href='https://github.com/alvara'><GitHub className='icon'/></a>
+        <a href='https://github.com/alvara' target="_blank"><GitHub className='icon'/></a>
       </div>
     </div>
   )
--- src/components/contact/contact.jsx
+++ src/components/contact/contact.jsx
@@ -7,18 +7,18 @@ export default function Contact() {
         <h2>Contact</h2>
 
       <div className='contact-form'>
-        <p>Send a message and let's get in touch!</p>
-        <Socials />
-        <form action='https://getform.io/f/cbea0041-d188-40df-ae75-32bafd02ca96' method='post'>
-          <h3>Contact Form</h3>
-          <label for='name'>Name</label>
-          <input type='text' placeholder='Name' id='name' name='name' />
-          <label for='name'>Name</label>
-          <input type='email' placeholder='Email' id='email' name='email' />
-          <label for='name'>Message</label>
-          <textarea placeholder='Message' />
-          <button type='submit'>Submit</button>
-        </form>
+          <form action='https://getform.io/f/cbea0041-d188-40df-ae75-32bafd02ca96' method='post'>
+            <label for='name'>Name</label>
+            <input type='text' placeholder='Name' id='name' name='name' />
+            <label for='name'>Email</label>
+            <input type='email' placeholder='Email' id='email' name='email' />
+            <label for='name'>Message</label>
+            <textarea placeholder='Message' />
+            <button type='submit'>Submit</button>
+          </form>
+          <h4>Connect On My Socials</h4>      
+          <Socials />
+
       </div>
     </div>
   )
