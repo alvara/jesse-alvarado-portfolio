1c64d6e92b52e1dcbc19b3e14c8b37124975117e
Jesse Alvarado <contact@jessealvarado.com>
2021-12-15 13:44:58 +0900

    create about page basic text only

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,10 +1,16 @@
+import { Container, Row, Col } from 'react-bootstrap';
 import './About.scss';
 
 export default function About() {
   return (
-    <div className='about'>
+    <Container className='about'>
       <h2>About</h2>
-      
-    </div>
+      <div className="about-text">
+        <p>I'm a team-oriented fullstack developer with a background in business and information technology.</p>
+        <p>I graduated Western Washington University with a BA in Management Information Systems in 2016, and worked as a software engineer at a private laser systems import company in Japan for 5 years. Since it was a small company of only 10 employees, I juggled many roles and responsibilities, from managing the internal network and security, providing onsite software support for our clients, created the homepage with a portfolio of over 100 products, and led the companies first digital marketing campaigns.</p>
+        <p>For me, the challenge of pushing myself to learn new skills in other fields has been very beneficial as it allows me to weave learned knowledge and apply them to business challenges.</p>
+        <p>I am currently focusing on web projects using Go/Golang, Google Cloud Platform (GCP), and modern frontend frameworks such as React and Svelte.</p>
+      </div>
+    </Container>
   )
 }
--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -3,18 +3,18 @@ import './Header.scss';
 
 export default function Header() {
   return (
-    <Container className='header' id='header' >
-        <div className="header-info">
-          <h1>Hi, I'm Jesse</h1>
-          <span className='subtitle'>I'm a Golang Developer<br /> based in Tokyo</span>
-        </div>
+    <Container className='header' id='header' > 
+       
       <Row>
-        <Col sm={5}>
+        <Col sm={12} md={5} lg={4}>
           <div className="left">
-          
+            <div className="header-text">
+              <h1>Hi, I'm Jesse</h1>
+              <span className='subtitle'>I'm a Golang Developer<br /> based in Tokyo</span>
+            </div>
           </div>
         </Col>
-          <Col sm={7}>
+          <Col sm={12} md={7} lg={8}>
             {/* <div className="img-bg"></div> */}
             <div className="right">
               <img src="personal-pic.png" alt="" className='personal-pic' />
