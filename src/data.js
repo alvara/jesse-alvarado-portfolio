e5ea1e15310f90e4e32c6eef354e9fdb55092e40
Jesse Alvarado <contact@jessealvarado.com>
2021-12-17 09:21:03 +0900

    add draft of erp portfolio item

--- src/data.js
+++ src/data.js
@@ -25,7 +25,7 @@ export const portfolio = [
       },
       {
         title: "The Challenge",
-        details: "A company was in business for over 20 years. During this time, many of their critical sales documents such as invoices, purchase orders, and price quotes were created in various spreadsheet programs (Excel, OpenOffice, LibreOffice), and all in a wide range of formats. This made it difficult to analyze important sales information for management.",
+        details: "The company needed a way to standardize the workflow based on their current workflow. After exploring several out-of-the-box solutions, it was decided that neither option matched the workflow of a Japanese company that required a certain hanko (seal) workflow to be followed, so a custom system was needed. ",
         top_imgs: [],
         right_imgs: [],
         bottom_imgs: [],
@@ -33,7 +33,7 @@ export const portfolio = [
       },
       {
         title: "The Solution",
-        details: "A company was in business for over 20 years. During this time, many of their critical sales documents such as invoices, purchase orders, and price quotes were created in various spreadsheet programs (Excel, OpenOffice, LibreOffice), and all in a wide range of formats. This made it difficult to analyze important sales information for management.",
+        details: "A micro erp system was necessary to match the current workflow of how sales documents were generated, while providing consistency so management can obtain and generate critical reports.",
         top_imgs: [],
         right_imgs: [],
         bottom_imgs: [],
@@ -42,14 +42,33 @@ export const portfolio = [
       {
         title: "Features",
         details: `<ul>\n
-        <li>test</li>\n
-        <li>test1</li>\n
+        <li>A user account system that allows certain users to approve the status of documents depending on their status in the workflow.</li>\n
+        <li>Generate document ID's according to the company's current incremental formula.</li>\n
+        <li>Have a system wide currency conversion for calculating the profit margins to determine the sales price to customers.</li>\n
+        <li>Print a preview of the sales documents on the fly that matched the company's preferred style and look.</li>\n
+        <li>Automatic backup system to export documents to the company's internal server for outside users to access without needing to use or learn the ERP system. </li>\n
         </ul>`,
         top_imgs: [],
         right_imgs: [],
         bottom_imgs: [],
         left_imgs: [],
       },
+      {
+        title: "Features",
+        details: `After a little over a year of development, the system satisfied all requirements laid out by the company, and was able to generate all important documents needed.`,
+        top_imgs: [],
+        right_imgs: [],
+        bottom_imgs: [],
+        left_imgs: [],
+      },
+      {
+        title: "Source",
+        details: `Because an NDA was signed, I am unable to disclose the source code for this project. However, I have created a demo of the project with all sensitive information removed that can be viewed here.`,
+        top_imgs: [],
+        right_imgs: [],
+        bottom_imgs: [],
+        left_imgs: [],
+      }
     ] 
   }
 ]
